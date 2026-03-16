5c879c4d7bf0db9ff1a6858ac543f904ea492696
SURJITH JAYAKUMAR <jksurjith@gmail.com>
2026-03-16 10:01:43 +0530

    Add files via upload

---
+++ UC1BookMyStayApp.java
@@ -0,0 +1,11 @@
+public class UC1BookMyStayApp {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("     Welcome to Book My Stay App    ");
+        System.out.println("Application Name : Hotel Booking Management System");
+        System.out.println("Application started successfully!");
+
+    }
+}
